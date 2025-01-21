178de9476e02630364e54498b2d7bef29d7b963e
BBAMO <gao0708@naver.com>
2025-01-21 13:00:24 +0900

    델타타임

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     //컴포넌트를 캐시 처리할 변수
-    private Transform tr;
+    //[SerializeField] - 접근해야 하는 컴포넌트는 반드시 변수에 할당한 후 사용
+    [SerializeField] private Transform tr;
+    //이동속도 변수(public으로 선언되어 인스펙터 뷰에 노출)
+    public float moveSpeed = 10.0f;
 
     void Start()
     {
@@ -24,6 +27,7 @@ public class PlayerCtrl : MonoBehaviour
         //transform.position += new Vector3(0, 0, 1);
 
         //정규화 벡터를 이용한 코드
-        tr.position += Vector3.forward * 1;
+        //tr.position += Vector3.forward * 1;
+        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
     }
 }
