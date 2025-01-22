7c88d8501b7bdb7edf66f0a71ccc50c4c49f112e
BBAMO <gao0708@naver.com>
2025-01-22 15:30:08 +0900

    player ctrl

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,9 +6,11 @@ public class PlayerCtrl : MonoBehaviour
 {
     //컴포넌트를 캐시 처리할 변수
     //[SerializeField] - 접근해야 하는 컴포넌트는 반드시 변수에 할당한 후 사용
-    [SerializeField] private Transform tr;
+    private Transform tr;
     //이동속도 변수(public으로 선언되어 인스펙터 뷰에 노출)
     public float moveSpeed = 10.0f;
+
+    public float turnSpeed = 80.0f;
 
     void Start()
     {
@@ -19,15 +21,20 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
         float v = Input.GetAxis("Vertical"); // -1.0f ~ 0.0f ~ +1.0f
+        float r = Input.GetAxis("Mouse X");
 
-        Debug.Log("h=" + h);
-        Debug.Log("v=" + v);
+        //Debug.Log("h=" + h);
+        //Debug.Log("v=" + v);
 
         // Transform 컴포넌트의 position 속성값을 변경
         //transform.position += new Vector3(0, 0, 1);
 
-        //정규화 벡터를 이용한 코드
-        //tr.position += Vector3.forward * 1;
-        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
+        // 전후좌우 이동 방향 벡터 계산
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        // Translate(이동 방향 * 속력 * Time.deltaTime)
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
+
+        tr. Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
