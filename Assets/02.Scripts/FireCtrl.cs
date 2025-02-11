f029ad93ec6e4dac012ff9672a53946d641cae5c
BBAMO <gao0708@naver.com>
2025-02-11 13:08:49 +0900

    sound

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 반드시 필요한 컴포넌트를 명시해 해당 컴포넌트가 삭제되는 것을 방지하는 어트리뷰트
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     // 총알 프리팹
     public GameObject bullet;
     // 총알 발사 좌표
     public Transform firePos;
+    // 총소리에 사용할 오디오 음원
+    public AudioClip fireSfx;
+
+    // AudioSource 컴포넌트를 저장할 변수
+    private new AudioSource audio;
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -25,6 +32,9 @@ public class FireCtrl : MonoBehaviour
         {
             // bullet 프리팹을 동적으로 생성
             Instantiate(bullet, firePos.position, firePos.rotation);
+
+            // 총소리 발생
+            audio.PlayOneShot(fireSfx, 1.0f);
         }
     }
 }
